dc611f4b533a6b6c59d34639c22c8353cf333ea6
LENOVO <LENOVO@LAPTOP-R5KFAO7Q>
2018-08-09 18:37:52 -0500

    elegir productos

--- src/Formulario/elegir_producto.java
+++ src/Formulario/elegir_producto.java
@@ -10,16 +10,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.table.DefaultTableModel;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author LENOVO
- */
 public class elegir_producto extends javax.swing.JDialog {
 
     public static venta ventas;
